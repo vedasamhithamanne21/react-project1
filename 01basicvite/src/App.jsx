652d1eb56474da6851813b489abc8124ff861f3f
Veda Samhitha Manne <vedasamhithamanne21>
2025-01-23 22:32:51 -0600

    adding the basic vite to the git

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube";
+
+function App(){
+  const username = "Veda";
+  return (
+    <>
+   <h1>Vite React App</h1>
+   <h1> Vite and react {""} app {2+2} </h1>
+    <Youtube/>
+    </>
+  )
+}
+export default App;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube(){
+    return(
+        <h1>Youtube React </h1>
+    )
+}
+export default Youtube
